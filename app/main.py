fadb84845de35356655bd89d06326a92be94f52c
Saivel1 <saivel.mezencev1@gmail.com>
2025-11-10 12:57:08 +0300

    fdfs

--- app/main.py
+++ app/main.py
@@ -43,9 +43,9 @@ async def lifespan(app: Litestar):
         drop_pending_updates=False
     )
     print(f"Webhook установлен: {settings.WEBHOOK_URL}")
-    async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.drop_all)
-        await conn.run_sync(Base.metadata.create_all)
+    # async with engine.begin() as conn:
+    #     await conn.run_sync(Base.metadata.drop_all)
+    #     await conn.run_sync(Base.metadata.create_all)
 
     yield
 
