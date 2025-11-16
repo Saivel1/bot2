9d58533efb7b793c8545aafbb4565995a75b9ceb
Saivel1 <saivel.mezencev1@gmail.com>
2025-11-16 20:01:15 +0300

    new retry mod

--- marz/backend.py
+++ marz/backend.py
@@ -4,28 +4,6 @@ import asyncio
 from config_data.config import settings as s
 from logger_setup import logger
 from typing import Optional, Dict, Any
-import json
-from functools import wraps
-import ssl
-
-def retry_on_failure(max_attempts=3, delay=1):
-    """Декоратор для retry"""
-    def decorator(func):
-        @wraps(func)
-        async def wrapper(*args, **kwargs):
-            for attempt in range(max_attempts):
-                try:
-                    return await func(*args, **kwargs)
-                except (aiohttp.ClientError, asyncio.TimeoutError) as e:
-                    if attempt < max_attempts - 1:
-                        logger.warning(f"Retry {attempt + 1}/{max_attempts}: {e}")
-                        await asyncio.sleep(delay * (attempt + 1))
-                    else:
-                        logger.error(f"Все попытки исчерпаны: {e}")
-                        raise
-            return None
-        return wrapper
-    return decorator
 
 
 
@@ -41,7 +19,7 @@ class MarzbanClient:
 
     async def _make_request(self, method: str, endpoint: str, **kwargs) -> Optional[Dict[str, Any]]:
         max_attempts = 5
-        delay = 0.5
+        delay = 1
         
         for attempt in range(max_attempts):
             try:
@@ -63,6 +41,8 @@ class MarzbanClient:
                             # Проверяем, это fallback или реальный 404
                             if 'kittenx' in body or 'Not Found' in body:
                                 logger.warning(f"Xray fallback, retry {attempt + 1}/{max_attempts}")
+                                if attempt % 3 == 0 and attempt != 0:
+                                    await asyncio.sleep(delay * 20 * (attempt + 1))
                                 await asyncio.sleep(delay * (attempt + 1))
                                 continue
                             else:
@@ -79,19 +59,20 @@ class MarzbanClient:
                             return None
             
             except (aiohttp.ClientError, asyncio.TimeoutError) as e:
+                e = str(e)
                 if attempt < max_attempts - 1:
-                    logger.warning(f"Retry {attempt + 1}/{max_attempts}: {e}")
+                    logger.warning(f"Retry {attempt + 1}/{max_attempts}: {e[:20]}")
                     await asyncio.sleep(delay * (attempt + 1))
                 else:
-                    logger.error(f"Все попытки исчерпаны: {e}")
+                    logger.error(f"Все попытки исчерпаны: {e[:20]}")
                     return None
         
         return None
     
     async def _get_token(self) -> str:
         """Получить токен с retry"""
-        max_attempts = 5
-        delay = 0.5
+        max_attempts = 15
+        delay = 1
         
         data = {
             "username": self.user,
@@ -110,14 +91,18 @@ class MarzbanClient:
                             return json_data["access_token"]
                         elif attempt < max_attempts - 1:
                             logger.warning(f"Token retry {attempt + 1}: статус {response.status}")
-                            await asyncio.sleep(delay * (attempt + 1))
+                            if attempt % 5 == 0:
+                                await asyncio.sleep(delay * 20 * (attempt + 1))
+                            else:
+                                await asyncio.sleep(delay * (attempt + 1))
                             continue
                         else:
                             raise Exception(f"Failed to get token: {response.status}")
             
             except aiohttp.ClientError as e:
+                e = str(e)
                 if attempt < max_attempts - 1:
-                    logger.warning(f"Token retry {attempt + 1}: {e}")
+                    logger.warning(f"Token retry {attempt + 1}: {e[:20]}")
                     await asyncio.sleep(delay * (attempt + 1))
                 else:
                     logger.error(f"Не удалось получить токен после {max_attempts} попыток")
@@ -136,35 +121,34 @@ class MarzbanClient:
         }
         return await self._make_request(method="GET", endpoint=f"/api/user/{user_id}", headers=headers)
 
-        """Получить информацию о пользователе"""
+        # """Получить информацию о пользователе"""
     
-        try:
-            token = await self._get_token()
-            headers = {
-                "accept": "application/json",
-                "Authorization": f"Bearer {token}"
-            }
+        # try:
+        #     token = await self._get_token()
+        #     headers = {
+        #         "accept": "application/json",
+        #         "Authorization": f"Bearer {token}"
+        #     }
 
-            async with aiohttp.ClientSession(timeout=self.timeout) as session:
-                async with session.get(
-                    url=f"{self.base_url}/api/user/{user_id}",
-                    headers=headers,
-                    ssl=False
-                ) as response:
+        #     async with aiohttp.ClientSession(timeout=self.timeout) as session:
+        #         async with session.get(
+        #             url=f"{self.base_url}/api/user/{user_id}",
+        #             headers=headers,
+        #             ssl=False
+        #         ) as response:
                     
-                    if response.status in (200, 201):
-                        json_data = await response.json()
-                        logger.debug(f"Пользователь {user_id} получен в функции get_user")
-                        return json_data
-                    else:
-                        logger.warning(f"Ошибка в получении пользователя {user_id}: {response.status}")
-                        return None
+        #             if response.status in (200, 201):
+        #                 json_data = await response.json()
+        #                 logger.debug(f"Пользователь {user_id} получен в функции get_user")
+        #                 return json_data
+        #             else:
+        #                 logger.warning(f"Ошибка в получении пользователя {user_id}: {response.status}")
+        #                 return None
                     
-        except Exception as e:
-            logger.error(f"Исключение при получении пользователя {user_id}: {e}")
-            return None
+        # except Exception as e:
+        #     logger.error(f"Исключение при получении пользователя {user_id}: {e}")
+        #     return None
     
-    @retry_on_failure(max_attempts=3, delay=2)
     async def modify_user(self, user_id: str, expire: int):
         """Изменить данные пользователя"""
         try:
@@ -173,32 +157,31 @@ class MarzbanClient:
                 "accept": "application/json",
                 "Authorization": f"Bearer {token}"
             }
-
-            connector = aiohttp.TCPConnector(ssl=False)
             
             data = {"expire": expire}
             
-            async with aiohttp.ClientSession(timeout=self.timeout) as session:
-                async with session.put(
-                    url=f"{self.base_url}/api/user/{user_id}",
-                    headers=headers,
-                    json=data
-                ) as response:
+            return await self._make_request(method="PUT", endpoint=f'/api/user/{user_id}', headers=headers, json=data)
+
+            # async with aiohttp.ClientSession(timeout=self.timeout) as session:
+            #     async with session.put(
+            #         url=f"{self.base_url}/api/user/{user_id}",
+            #         headers=headers,
+            #         json=data
+            #     ) as response:
                     
-                    if response.status in (200, 201):
-                        json_data = await response.json()
-                        logger.debug(f"Пользователь {user_id} изменён")
-                        return json_data
-                    else:
-                        logger.warning(f"Ошибка в редактировании пользователя {user_id}: {response.status}")
-                        return None
+            #         if response.status in (200, 201):
+            #             json_data = await response.json()
+            #             logger.debug(f"Пользователь {user_id} изменён")
+            #             return json_data
+            #         else:
+            #             logger.warning(f"Ошибка в редактировании пользователя {user_id}: {response.status}")
+            #             return None
                     
         except Exception as e:
             logger.error(f"Исключение при редактировании пользователя {user_id}: {e}")
             return None
     
 
-    @retry_on_failure(max_attempts=3, delay=2)
     async def create_user(self, username: str) -> Optional[Dict[str, Any]]:
         """Создать нового пользователя"""
         try:
@@ -222,30 +205,30 @@ class MarzbanClient:
                     "vless": ["VLESS TCP VISION REALITY", "NODE1_REALITY"]
                 }
             }
-            
-            async with aiohttp.ClientSession(timeout=self.timeout) as session:
-                async with session.post(
-                    url=f"{self.base_url}/api/user",
-                    headers=headers,
-                    json=data, 
-                    ssl=False
-                ) as response:
-                    if response.status in (200, 201):
-                        json_data = await response.json()
-                        json_str = json.dumps(json_data)
 
-                        logger.debug(f"Пользователь {username} создан: {json_str[:25]}")
-                        return json_data
-                    else:
-                        error_text = await response.text()
-                        logger.warning(f"Ошибка в создании пользователя: {response.status}, {error_text}")
-                        return None
+            return await self._make_request(method="POST", endpoint="/api/user", headers=headers, json=data)
+            
+            # async with aiohttp.ClientSession(timeout=self.timeout) as session:
+            #     async with session.post(
+            #         url=f"{self.base_url}/api/user",
+            #         headers=headers,
+            #         json=data, 
+            #     ) as response:
+            #         if response.status in (200, 201):
+            #             json_data = await response.json()
+            #             json_str = json.dumps(json_data)
+
+            #             logger.debug(f"Пользователь {username} создан: {json_str[:25]}")
+            #             return json_data
+            #         else:
+            #             error_text = await response.text()
+            #             logger.warning(f"Ошибка в создании пользователя: {response.status}, {error_text}")
+            #             return None
                     
         except Exception as e:
             logger.error(f"Исключение при создании пользователя {username}: {e}")
             return None
     
-    @retry_on_failure(max_attempts=3, delay=2)
     async def create_user_options(self, username: str, id: str | None = None, inbounds: list | None = None, expire: int | None = None) -> Optional[Dict[str, Any]]:
         """Создать нового пользователя"""
         try:
@@ -278,32 +261,33 @@ class MarzbanClient:
             if expire:
                 data['expire'] = expire
             
-            async with aiohttp.ClientSession(timeout=self.timeout) as session:
-                async with session.post(
-                    url=f"{self.base_url}/api/user",
-                    headers=headers,
-                    json=data,
-                    ssl=False
-                ) as response:
-                    
-                    if response.status in (200, 201):
-                        json_data = await response.json()
-                        json_str = json.dumps(json_data)
 
-                        logger.debug(f"Пользователь {username} создан: {json_str[:25]}")
-                        return json_data
-                    else:
-                        error_text = await response.text()
-                        logger.warning(f"Ошибка в создании пользователя: {response.status}, {error_text}")
-                        return {"status": 409}
+            return await self._make_request(method="POST", endpoint="/api/user", headers=headers, json=data)
+        
+            # async with aiohttp.ClientSession(timeout=self.timeout) as session:
+            #     async with session.post(
+            #         url=f"{self.base_url}/api/user",
+            #         headers=headers,
+            #         json=data,
+            #     ) as response:
+                    
+            #         if response.status in (200, 201):
+            #             json_data = await response.json()
+            #             json_str = json.dumps(json_data)
+
+            #             logger.debug(f"Пользователь {username} создан: {json_str[:25]}")
+            #             return json_data
+            #         else:
+            #             error_text = await response.text()
+            #             logger.warning(f"Ошибка в создании пользователя: {response.status}, {error_text}")
+            #             return {"status": 409}
                     
         except Exception as e:
             logger.error(f"Исключение при создании пользователя {username}: {e}")
             return None
 
 
-    @retry_on_failure(max_attempts=3, delay=2)
-    async def delete_user(self, username: str) -> bool:
+    async def delete_user(self, username: str) -> dict | None:
         """Удалить пользователя"""
         try:
             token = await self._get_token()
@@ -311,21 +295,23 @@ class MarzbanClient:
                 "accept": "application/json",
                 "Authorization": f"Bearer {token}"
             }
+
+            return await self._make_request(method="DELETE", endpoint=f'/api/user/{username}', headers=headers)
             
-            async with aiohttp.ClientSession(timeout=self.timeout) as session:
-                async with session.delete(
-                    url=f"{self.base_url}/api/user/{username}",
-                    headers=headers,
-                    ssl=False
-                ) as response:
+            # async with aiohttp.ClientSession(timeout=self.timeout) as session:
+            #     async with session.delete(
+            #         url=f"{self.base_url}/api/user/{username}",
+            #         headers=headers,
+            #         ssl=False
+            #     ) as response:
                     
-                    if response.status in (200, 204):
-                        logger.debug(f"Пользователь {username} удалён")
-                        return True
-                    else:
-                        logger.warning(f"Ошибка при удалении пользователя {username}: {response.status}")
-                        return False
+            #         if response.status in (200, 204):
+            #             logger.debug(f"Пользователь {username} удалён")
+            #             return True
+            #         else:
+            #             logger.warning(f"Ошибка при удалении пользователя {username}: {response.status}")
+            #             return False
                     
         except Exception as e:
             logger.error(f"Исключение при удалении пользователя {username}: {e}")
-            return False
+            return None
