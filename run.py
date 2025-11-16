24ac838db4bfbc560a15f31e3e511e43857fb693
Saivel1 <saivel.mezencev1@gmail.com>
2025-11-16 17:19:42 +0300

    loop

--- run.py
+++ run.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
         address="127.0.0.1",
         port=8000,
         workers=1,
-        loop=Loops.uvloop,
+        loop=Loops.asyncio,
         log_enabled=True,
         interface=Interfaces.ASGI,
         reload=True
